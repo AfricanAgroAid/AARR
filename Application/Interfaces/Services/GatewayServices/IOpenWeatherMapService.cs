15db1412f681afe3f74fa20b6887c12d8dda31e6
SalamBhai <saulasheriffdeen@gmail.com>
2022-10-26 21:13:36 +0100

    Commit For The Open Weather Map Service Endpoints

--- Application/Interfaces/Services/GatewayServices/IOpenWeatherMapService.cs
+++ Application/Interfaces/Services/GatewayServices/IOpenWeatherMapService.cs
@@ -4,7 +4,7 @@ namespace Application.Interfaces.Services.GatewayServices;
 
 public interface IOpenWeatherMapService
 {
-    Task<GeocodingResponseModels> GetCityCoordinates(string cityName, int responseLimit = 1);
+    Task<GeocodingResponseModels> GetCityCoordinates(string cityName, int responseLimit = 1,string countryCode = "ng");
     Task<DaysClimaticForecastResponseModel> GetDaysClimaticForecast(decimal lat, decimal lon, int daysCount = 7);
     Task<CurrentWeatherForecastResponseModel> GetCurrentWeatherForecast(decimal lat, decimal lon, string countryCode = "ng");
 }
--- Application/JSONResponseModel/OpenWeatherMapServiceResponseModels/CurrentWeatherForecastResponseModel.cs
+++ Application/JSONResponseModel/OpenWeatherMapServiceResponseModels/CurrentWeatherForecastResponseModel.cs
@@ -1,5 +1,24 @@
+using System.Text.Json.Serialization;
+
 namespace Application.JSONResponseModel.OpenWeatherMapServiceResponseModels;
 
 public class CurrentWeatherForecastResponseModel
 {
+    [JsonPropertyName("coord")]
+    public Coordinates Coordinates { get; set; }
+
+
+}
+
+public class Coordinates
+{
+    [JsonPropertyName("lon")]
+    public string Longitude { get; set; }
+    [JsonPropertyName("lat")]
+    public string Latitude { get; set; }
+}
+
+public class WeatherInformation
+{
+
 }
