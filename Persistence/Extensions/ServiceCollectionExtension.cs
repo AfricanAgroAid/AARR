c144779722b99bc15fb7382f196733a689a8519f
SalamBhai <saulasheriffdeen@gmail.com>
2022-10-28 18:04:54 +0100

    Review For The Num Look Up Service

--- Persistence/Extensions/ServiceCollectionExtension.cs
+++ Persistence/Extensions/ServiceCollectionExtension.cs
@@ -1,5 +1,28 @@
+using Application.Interfaces;
+using Application.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Context;
+using Persistence.Repositories;
+
 namespace Persistence.Extensions;
 
 public static class ServiceCollectionExtension
 {
+     public static IServiceCollection AddDbConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("AARRConnectionString");
+        services.AddDbContext<ApplicationContext>(option => 
+        option.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+        return services;
+    }
+
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+       services.AddScoped<IFarmRepository, FarmRepository>();
+        services.AddScoped<IFarmerRepository, FarmerRepository>();
+        services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        return services;
+    }
 }
